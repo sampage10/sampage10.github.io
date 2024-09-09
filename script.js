19796139af57a21bb53d68d39c3e4245905900a1
sampage10 <134566800+sampage10@users.noreply.github.com>
2024-09-09 14:54:44 -0400

    darkmode button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById('theme').addEventListener('click', function(){
     document.body.classList.toggle('dark-mode');
 })
+
+const themeButton = document.getElementById('theme');
+
+function updateIcon() {
+    const iconClass = body.classList.contains('darkmode') ? 'fa-sun' : 'fa-mood';
+    themeButton.innerHTML = `<i class="fa-regular ${iconClass}"></i>`;
+}
