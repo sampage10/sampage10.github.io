93a16318c732327321fd8824d0cb264fd6da79ba
sampage10 <134566800+sampage10@users.noreply.github.com>
2024-09-09 13:31:09 -0400

    darkmode event listener

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('theme').addEventListener(click, function(){
+document.getElementById('theme').addEventListener('click', function(){
     document.body.classList.toggle('dark-mode');
 })
