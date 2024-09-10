05fdefe20be657c4c63f035e8e908dd21a5895e7
sampage10 <134566800+sampage10@users.noreply.github.com>
2024-09-09 21:28:31 -0400

    dark mode icons

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById('theme').addEventListener('click', function(){
     document.body.classList.toggle('dark-mode');
+    updateIcon();
 })
 
 const themeButton = document.getElementById('theme');
 
 function updateIcon() {
-    const iconClass = body.classList.contains('darkmode') ? 'fa-sun' : 'fa-moon';
+    const iconClass = body.classList.contains('dark-mode') ? 'fa-sun' : 'fa-moon';
     themeButton.innerHTML = `<i class="fa-regular ${iconClass}"></i>`;
 }
